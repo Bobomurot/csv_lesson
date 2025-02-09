d193974aacf90ad769370196bb4de4b2864d4971
Bobomurot <107930349+Bobomurot@users.noreply.github.com>
2025-02-09 20:06:05 +0900

    File readre
    New change

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import csv
 def get_country_column(file_name: str)-> list:
     """
     Bu funksiya fayl nomini qabul qiladi. Ushbu fayldan mamlakatlar(country) ning ro'yxatini chiqaring.
@@ -6,4 +8,9 @@ def get_country_column(file_name: str)-> list:
     Returns:
         list
     """
-    return 0
+    f = open("data.csv", "r")
+    read_csv = csv.reader(f)
+    for line in read_csv:
+        print(line[3])
+a = get_country_column("data.csv")
+print(a)
